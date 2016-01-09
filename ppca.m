cb33f350ec6715150f30d430376165a140ec160b
Pierre Petitbon <pierre.petitbon@gmail.com>
2016-01-09 19:10:59 +0100

    improved code readability

--- ppca.m
+++ ppca.m
@@ -2,6 +2,7 @@ function ppca_example
     filename = 'data/virus3.dat';
     T = importdata(filename);
     q = 2;
+
     [W, sigma] = ppca_from_data(T, q);
     X = ppca_latent(T, W, sigma);
     ppca_plot2d(X);
@@ -13,14 +14,13 @@ function [W, sigma] = ppca_from_data(T, q)
     for j = 1:d
         mu(j) = mean(T(:,j));
     endfor
-    
     S = zeros(d);
     for n = 1:N
         S = S + (T(n,:)' - mu') * (T(n,:)' - mu')';
     endfor
     S = 1/N * S;
 
-    [W, sigma] = ppca_from_covariance(S, d, q);
+    [W, sigma] = ppca_from_covariance(S, q);
 endfunction
 
 function X = ppca_latent(T, W, sigma)
@@ -28,34 +28,35 @@ function X = ppca_latent(T, W, sigma)
     [~, q] = size(W);
 
     M = W'*W + sigma^2 * eye(q);
-    
     for j = 1:d
         mu(j) = mean(T(:,j));
     endfor
-    
     for i = 1:N
         Tnorm(i,:) = T(i,:) - mu;
     endfor
+
     X = inv(M) * W' * Tnorm';
 endfunction
 
 function ppca_plot2d(X)
     [~, N] = size(X);
+
     plot(X(1,:), X(2,:), "linestyle", 'none');
     for i = 1:N
         text(X(1,i), X(2,i), num2str(i));
     endfor
 endfunction
 
-function [W, sigma] = ppca_from_covariance(S, d, q)
+function [W, sigma] = ppca_from_covariance(S, q)
+    [d, ~] = size(S);
+
     [Wpca, lambda] = eig(S);
     lambda = diag(lambda);
     [lambda, i] = sort(lambda, 'descend');
     Wpca = Wpca(:,i);
-    q = 2;
-    
     U = Wpca(:,1:q);
     L = diag(lambda)(1:q, 1:q);
+
     sigma = sqrt(1/(d-q) * sum(lambda(q+1:d)));
     W = U * sqrt(L - sigma^2*eye(q));
 endfunction
