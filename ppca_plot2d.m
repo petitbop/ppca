317fc4bebfc4313b6326d9983fa41cbb5240dc6b
Pierre Petitbon <pierre.petitbon@gmail.com>
2016-01-14 17:15:25 +0100

    made axis limits larger

--- ppca_plot2d.m
+++ ppca_plot2d.m
@@ -1,8 +1,7 @@
 function ppca_plot2d(X)
     [~, N] = size(X);
 
-    % plot(X(1,:), X(2,:), "linestyle", 'none');
-    axis([-3, 2, -2, 1.5])
+    axis([-3, 3, -3, 3])
     for i = 1:N
         text(X(1,i), X(2,i), num2str(i));
     end
